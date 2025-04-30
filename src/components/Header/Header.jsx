90bf6adbe2d5c38703ab7006733b7fd1d6527102
SerhiiMonzhelesov <129993794+SerhiiMonzhelesov@users.noreply.github.com>
2025-04-30 22:41:17 +0300

    add typewriter

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,41 @@
 import { NavLink, Link } from "react-router-dom";
 import { StyledHeader } from "./Header.styled";
 import { HeaderContainer } from "./Header.styled";
+import { useEffect, useState } from "react";
 
 function Header() {
+  const fullText = "Serhii Monzhelesov";
+  const [displayedText, setDisplayedText] = useState("");
+  const [showCursor, setShowCursor] = useState(true);
+
+  useEffect(() => {
+    const typeNext = (index) => {
+      if (index <= fullText.length) {
+        setDisplayedText(fullText.slice(0, index));
+        //setTimeout(() => typeNext(index + 1), 120);
+        const randomDelay = Math.floor(Math.random() * 1) + 150; // від 100 до 400 мс
+        setTimeout(() => typeNext(index + 1), randomDelay);
+      } else {
+        setTimeout(() => setShowCursor(false), 2000);
+      }
+    };
+
+    typeNext(1); // починаємо з першої букви
+
+    return () => {}; // нічого не очищаємо
+  }, []);
+
   return (
     <StyledHeader>
       <HeaderContainer>
+        <p className="typewriter">
+          {displayedText.split("").map((char, idx) => (
+            <span className="letter" key={idx}>
+              {char === " " ? "\u00A0" : char}
+            </span> // правильне виведення пробілу
+          ))}
+          {showCursor && <span className="cursor"></span>}
+        </p>
         <nav>
           <Link to={"https://www.google.com"} target="blank">
             Portfolio
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Container from "../../shared/Container/Container";
 
 export const StyledHeader = styled.header`
@@ -7,17 +7,50 @@ export const StyledHeader = styled.header`
   border-bottom: 1px solid #c4b7b7;
 `;
 
+const blink = keyframes`
+  50% {
+    opacity: 0;
+  }
+`;
+
 export const HeaderContainer = styled(Container)`
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: row;
+  justify-content: space-between;
+
+  p {
+    font-size: 24px;
+    font-weight: 500;
+    //color: white;
+  }
+
+  .typewriter {
+    display: inline-flex;
+    align-items: center;
+    position: relative;
+  }
+
+  .letter {
+    opacity: 1;
+    transition: opacity 0.3s;
+  }
+
+  .cursor {
+    width: 1ch;
+    height: 1.2em;
+    background-color: black;
+    margin-left: 2px;
+    animation: ${blink} 450ms step-start infinite;
+  }
 
   nav {
     display: flex;
+    align-items: center;
     column-gap: 20px;
   }
 
   a {
-    color: white;
+    //color: white;
   }
 
   a:hover {
